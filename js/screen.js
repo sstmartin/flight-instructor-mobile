fa9733ff800808cc5bbd032d9db4819aaa015d85
Scott St. Martin <sjsm3@gatech.edu>
2014-05-05 10:21:15 -0400

    initial upload
    first upload of the application

---
+++ js/screen.js
@@ -0,0 +1,24 @@
+/*
+ * init_screen(type)
+ * Purpose:
+ * Display the appropriate HTML divs for the given screen type.
+ */
+function init_screen(type){
+    switch (type){
+        case 'n':
+            document.getElementById("n_footer").style.visibility="visible";
+            document.getElementById("m_footer").style.visibility="hidden";
+            document.getElementById("l_footer").style.visibility="hidden";
+            break;
+        case 'm':
+            document.getElementById("n_footer").style.visibility="hidden";
+            document.getElementById("m_footer").style.visibility="visible";
+            document.getElementById("l_footer").style.visibility="hidden";
+            break;
+        case 'l':
+            document.getElementById("n_footer").style.visibility="hidden";
+            document.getElementById("m_footer").style.visibility="hidden";
+            document.getElementById("l_footer").style.visibility="visible";
+            break;
+    }
+}
