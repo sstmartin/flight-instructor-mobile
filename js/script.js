a3cfa2dc1fd4237f5bd187f9d55ae40aa3d4348d
Scott St. Martin <sjsm3@gatech.edu>
2014-05-15 19:46:18 -0400

    Everything
    is beautiful

--- js/script.js
+++ js/script.js
@@ -1,44 +1,46 @@
-$(document).ready(function(){
-    
+$(document).ready(function() {
+
     //Function calls to initialize data
     drop_table();
     //update_event_list();
-    
+
     //Refreshes the Event List Frame
     function select_events() {
         events("SELECT * FROM EVENTS;", function(get_events) {
             current_events = get_events;
-            
+
             //Resets the option dropdown
             $('#m_add_parent option').remove();
             $('#m_add_parent').append($('<option></option>')
                     .text('No Parent'));
-            
+
             //Resets the table
             $('#m_entry_table tbody tr').remove();
-            
+
             //Repopulates the table and dropbown
-            for(var i = 0; i < current_events.length; i++) {
-                
-                if(current_events[i][3] === 'No Parent') {
+            for (var i = 0; i < current_events.length; i++) {
+
+                if (current_events[i][3] === 'No Parent') {
                     $('#m_add_parent').append($('<option></option>')
                             .text(current_events[i][0]));
                 }
-                
+
                 $('#m_entry_table tbody').append(
                         '<tr><td>' + current_events[i][4] + '</td>' +
                         '<td>' + current_events[i][0] + '</td>' +
-                        '<td>' + current_events[i][1] + '</td>' + 
-                        '<td>' + current_events[i][2] + '</td>' + 
-                        '<td>' + current_events[i][3] + '</td>' + 
-                        '<td><input type="checkbox" id="m_delete' + i + '" value="0" onClick= "this.value ^= true" ></td></tr>');
+                        '<td>' + current_events[i][1] + '</td>' +
+                        '<td>' + current_events[i][2] + '</td>' +
+                        '<td>' + current_events[i][3] + '</td>' +
+                        '<td><input type="checkbox" id="m_delete_' + current_events[i][0] + '" value="0" onClick= "this.value ^= true" class="deletestuff"></td></tr>');
             }
-            
+
+            //FIXES DROP DOWN DON'T TOUCH
+            $('#m_add_parent').val('No Parent');
             //$('#m_add_parent').val('No Parent');
-			$('#m_add_parent').val('No Parent').change();
+            +$('#m_add_parent').val('No Parent').change();
         });
     }
-    
+
     //When You Click Add Event
     $('#m-add').click(function() {
         //Grabs values for add table
@@ -47,20 +49,20 @@ $(document).ready(function(){
         gnd = $('#m_add_gnd').val();
         flight = $('#m_add_flight').val();
         parent = $('#m_add_parent').val();
-        
-        add_event(task,gnd,flight,parent,stage);
-        
+
+        add_event(task, gnd, flight, parent, stage);
+
         //Resets Values to 0 after added
         $('#m_add_stage').val('');
         $('#m_add_task').val('');
-        $('#m_add_gnd').prop('checked',false);
+        $('#m_add_gnd').prop('checked', false);
         $('#m_add_gnd').val('0');
-        $('#m_add_flight').prop('checked',false);
+        $('#m_add_flight').prop('checked', false);
         $('#m_add_flight').val('0');
         select_events();
-        
+
     });
-    
+
     $(':checkbox').click(function() {
         if ($(this).val() === '1') {
             $(this).val('0');
@@ -69,7 +71,7 @@ $(document).ready(function(){
             $(this).val('1');
         }
     });
-    
+
     //Event List Delete
     $('#m-delete').click(function() {
         $('<div>').simpledialog2({
@@ -81,9 +83,16 @@ $(document).ready(function(){
                 'OK': {
                     click: function() {
                         $('#buttonoutput').text('OK');
-                        $('#m_delete').each(function() {
-                           //if($(this).) 
+
+                        $('.deletestuff').each(function() {
+                            if ($(this).val() === '1') {
+                                sendthisin = $(this).attr("id").substring(9);
+
+                                delete_event(sendthisin);
+                            }
                         });
+
+                        select_events();
                     }
                 },
                 'Cancel': {
@@ -95,7 +104,5 @@ $(document).ready(function(){
                 }
             }
         });
-        
-        alert($("#m_delete0").val());
     });
 });
