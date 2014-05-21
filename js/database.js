88fb4c7e2d032336ea8a18ca21f77e688903dff7
briannemsick <brian.nemsick@gmail.com>
2014-05-21 18:43:34 -0400

    1st Screen, Proper Restructure

--- js/database.js
+++ js/database.js
@@ -1,43 +1,52 @@
-//Add a new task to the TASKS database
-function add_task(order,stage,task,gnd,flight,parent) {
+function add_entry(my_user,my_order, complete_gnd, date_gnd, complete_flight, date_flight, remarks) {
+    var db = openDatabase('flight_instructor_mobile', '1.0', 'Data Storage', 2 * 1024 * 1024);
+    db.transaction(function (tx) {
+        //First time condition
+        tx.executeSql('CREATE TABLE IF NOT EXISTS USERS (my_user, my_order, complete_gnd, date_gnd, complete_flight, date_flight, remarks)');
+        tx.executeSql('INSERT INTO USERS (my_user, my_order, complete_gnd, date_gnd, complete_flight, date_flight, remarks) VALUES ("' + my_user + '", "' + my_order + '", "' + complete_gnd + '", "' + date_gnd + '", "' + complete_flight + '", "' + date_flight + '", "' + remarks + '")');
+	});
+
+}
+
+function add_task(order, stage, task, gnd, flight, parent) {
     //Open
     var db = openDatabase('flight_instructor_mobile', '1.0', 'Data Storage', 2 * 1024 * 1024);
     //Write
-    db.transaction(function(tx) {
+    db.transaction(function (tx) {
         //First time condition
-	tx.executeSql('CREATE TABLE IF NOT EXISTS TASKS (my_order unique, stage, task unique, gnd, flight, parent)');
- 
-        tx.executeSql('INSERT INTO TASKS (my_order, stage, task, gnd, flight, parent) VALUES ("'+ order + '", "' + stage + '", "' + task + '", "' + gnd + '", "' + flight + '", "' + parent + '")');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS TASKS (my_order unique, stage, task, gnd, flight, parent)');
+
+        tx.executeSql('INSERT INTO TASKS (my_order, stage, task, gnd, flight, parent) VALUES ("' + order + '", "' + stage + '", "' + task + '", "' + gnd + '", "' + flight + '", "' + parent + '")');
     });
 }
 
-//Delete a task from the TASKS database
 function delete_task(my_order) {
     //Open
     var db = openDatabase('flight_instructor_mobile', '1.0', 'Data Storage', 2 * 1024 * 1024);
     //Write
-    db.transaction(function(tx) {
+    db.transaction(function (tx) {
         //Delete data
         tx.executeSql('DELETE FROM TASKS WHERE my_order = "' + my_order + '"');
     });
 }
 
-function tasks(query, callback){
-   var current_tasks = [];
+function tasks(query, callback) {
+    var current_tasks = [];
     var db = openDatabase('flight_instructor_mobile', '1.0', 'Data Storage', 2 * 1024 * 1024);
     db.transaction(function (tx) {
-      tx.executeSql(query, [], function(tx, results){
-         for(var i=0; i<results.rows.length; i++) {
-             current_tasks[i] = [results.rows.item(i).my_order,results.rows.item(i).stage,results.rows.item(i).task,results.rows.item(i).gnd,results.rows.item(i).flight, results.rows.item(i).parent];
-         }
-         callback(current_tasks);
-      });
-   });
-} 
+        tx.executeSql(query, [], function (tx, results) {
+            for (var i = 0; i < results.rows.length; i++) {
+                current_tasks[i] = [results.rows.item(i).my_order, results.rows.item(i).stage, results.rows.item(i).task, results.rows.item(i).gnd, results.rows.item(i).flight, results.rows.item(i).parent];
+            }
+            callback(current_tasks);
+        });
+    });
+}
 
-function drop_table(){
+function drop_table() {
     var db = openDatabase('flight_instructor_mobile', '1.0', 'Data Storage', 2 * 1024 * 1024);
-    db.transaction(function(tx) {
+    db.transaction(function (tx) {
         tx.executeSql('DROP TABLE TASKS');
+        tx.executeSql('DROP TABLE USERS');
     });
 }
--- js/screen_manager.js
+++ js/screen_manager.js
@@ -1,114 +1,175 @@
+//Define Variables
 var ORDER = 0;
 var STAGE = 1;
 var TASK = 2;
 var GND = 3;
 var FLIGHT = 4;
 var PARENT = 5;
+var MASTER_QUERY = "SELECT * FROM tasks ORDER BY my_order ASC;";
 
-/*
- * init_screen(type)
- * Purpose:
- * Display the appropriate HTML divs for the given screen type.
- */
-function init_screen(type){
-    switch (type){
-        case 'n':
-            document.getElementById("n_footer").style.visibility="visible";
-            document.getElementById("m_footer").style.visibility="hidden";
-            document.getElementById("l_footer").style.visibility="hidden";
-            
-            document.getElementById("m_main").style.visibility="hidden";
-            
-            break;
-        case 'm':
-            document.getElementById("n_footer").style.visibility="hidden";
-            document.getElementById("m_footer").style.visibility="visible";
-            document.getElementById("l_footer").style.visibility="hidden";
-            
-            document.getElementById("m_main").style.visibility="visible";
-            
-            break;
-        case 'l':
-            document.getElementById("n_footer").style.visibility="hidden";
-            document.getElementById("m_footer").style.visibility="hidden";
-            document.getElementById("l_footer").style.visibility="visible";
-            
-            document.getElementById("m_main").style.visibility="hidden";
-            
-            break;
+function screen(type) {
+    switch (type) {
+    case 'l':
+        document.getElementById("l_footer").style.display = '';
+        document.getElementById("m_footer").style.display = 'none';
+        document.getElementById("m_main").style.display = 'none';
+        document.getElementById("n_footer").style.display = 'none';
+        document.getElementById("n_main").style.display = 'none';
+        break;
+    case 'm':
+        document.getElementById("l_footer").style.display = 'none';
+        document.getElementById("m_footer").style.display = '';
+        document.getElementById("m_main").style.display = '';
+        document.getElementById("n_footer").style.display = 'none';
+        document.getElementById("n_main").style.display = 'none';
+        break;
+    case 'n':
+        document.getElementById("l_footer").style.display = 'none';
+        document.getElementById("m_footer").style.display = 'none';
+        document.getElementById("m_main").style.display = 'none';
+        document.getElementById("n_footer").style.display = '';
+        document.getElementById("n_main").style.display = '';
+
+        break;
     }
 }
 
-$(document).ready(function() {
-    
-    drop_table();
-
-    //Refreshes the task List Frame
-    function select_tasks() {
-        tasks("SELECT * FROM tasks ORDER BY my_order ASC;", function(get_tasks) {
-            current_tasks = get_tasks;
-
-            //Resets the option dropdown
-            $('#m_add_parent option').remove();
-            $('#m_add_parent').append($('<option></option>')
-                   .text('None'));
-
-            //Resets the table
-            $('#m_entry_table tbody tr').remove();
-
-            //Repopulates the table and dropbown
-            for (var i = 0; i < current_tasks.length; i++) {
-
-                if (current_tasks[i][PARENT] === 'None') {
-                    $('#m_add_parent').append($('<option></option>')
-                            .text(current_tasks[i][TASK]));
-                }
-
-                $('#m_entry_table tbody').append(
-                        '<tr><td>' + current_tasks[i][ORDER] + '</td>' +
-                        '<td>' + current_tasks[i][STAGE] + '</td>' +
-                        '<td>' + current_tasks[i][TASK] + '</td>' +
-			'<td>' + current_tasks[i][GND] + '</td>' +
-                        '<td>' + current_tasks[i][FLIGHT] + '</td>' +
-                        '<td>' + current_tasks[i][PARENT] + '</td>' +
-                        '<td><input type="checkbox" id="m_delete_' + current_tasks[i][ORDER] + '" value="0" onClick= "this.value ^= true" class = "delete_tasks"></td></tr>');
-            }
-
-            //Updates dropdown window
-            $('#m_add_parent').val('None');
-            $('#m_add_parent').val('None').change();
-        });
-    }
-
-    //When You Click Add task
-    $('#m_add').click(function() {
-        //Grabs values for add table
-	order = $('#m_add_order').val();
-        stage = $('#m_add_stage').val();
-        task = $('#m_add_task').val();
-        gnd = $('#m_add_gnd').val();
-        flight = $('#m_add_flight').val();
-        parent = $('#m_add_parent').val();
-
-	//Check for actual input
-	if (order != '' && stage != ''){
-        	add_task(order,stage,task,gnd, flight, parent);
-	}
-
-        //Resets Values to 0 after added
-	$('#m_add_order').val('');
+function reset(type) {
+    switch (type) {
+    case 'm':
+        $('#m_add_order').val('');
         $('#m_add_stage').val('');
         $('#m_add_task').val('');
         $('#m_add_gnd').prop('checked', false);
         $('#m_add_gnd').val('0');
         $('#m_add_flight').prop('checked', false);
         $('#m_add_flight').val('0');
-        select_tasks();
+        $('#m_add_parent').val('None');
+        $('#m_add_parent').val('None').change();
+        $('.m_select').each(function () {
+            $(this).prop('checked', false);
+            $(this).val('0');
+        });
+        break;
+    case 'n':
+        $('#n_name').val('');
+        $('#n_date').val('');
+        $('.n_gnd').each(function () {
+            $(this).prop('checked', false);
+            $(this).val('0');
+        });
+        $('.n_flight').each(function () {
+            $(this).prop('checked', false);
+            $(this).val('0');
+        });
+        $('.n_gnd').each(function () {
+            $(this).prop('checked', false);
+            $(this).val('0');
+        });
+        $('.n_flight_date').each(function () {
+            $(this).val('0');
+        });
+        $('.n_gnd_date').each(function () {
+            $(this).val('0');
+        });
+        $('.n_remarks').each(function () {
+            $(this).val('');
+        });
+        $('.n_select').each(function () {
+            $(this).prop('checked', false);
+            $(this).val('0');
+        });
+        break;
+    }
+}
 
+function submit(type) {
+    switch (type) {
+    case 'm':
+        order = $('#m_add_order').val();
+        stage = $('#m_add_stage').val();
+        task = $('#m_add_task').val();
+        gnd = $('#m_add_gnd').val();
+        flight = $('#m_add_flight').val();
+        parent = $('#m_add_parent').val();
+
+        //Check for actual input
+        if (order != '' && stage != '') {
+            add_task(order, stage, task, gnd, flight, parent);
+        }
+
+        reset('m');
+        break;
+    case 'n':
+        $('.n_select').each(function () {
+            if ($(this).val() === '1') {
+		user = $('#n_name').val();
+		order = $(this).attr("id").substring(9).replace( /(:|\.|\[|\])/g, "\\$1" );
+		complete_gnd = $('#n_gnd_' + order).val();
+		date_gnd = $('#n_gnd_date_' + order).val();
+		complete_flight = $('#n_flight_' + order).val();
+		date_flight = $('#n_flight_date_' + order).val();
+		remarks = $('#n_remarks_' + order).val();
+		order= $(this).attr("id").substring(9);
+		if ((date_gnd !== ''|| date_flight !== '') && user !== ''){
+			add_entry(user,order,complete_gnd,date_gnd,complete_flight,date_flight,remarks);
+		}
+            }
+        });
+	reset('n');
+        break;
+    }
+    select_tasks();
+}
+
+function select_tasks() {
+    tasks(MASTER_QUERY, function (get_tasks) {
+        current_tasks = get_tasks;
+
+        //Resets the option dropdown
+        $('#m_add_parent option').remove();
+        $('#m_add_parent').append($('<option></option>')
+            .text('None'));
+
+        //Resets the table
+        $('#m_entry_table tbody tr').remove();
+        $('#n_form tbody tr').remove();
+
+        //Repopulates the table and dropbown
+        for (var i = 0; i < current_tasks.length; i++) {
+
+            if (current_tasks[i][PARENT] === 'None') {
+                $('#m_add_parent').append($('<option></option>')
+                    .text(current_tasks[i][TASK]));
+            }
+
+            $('#m_entry_table tbody').append(
+                '<tr><td>' + current_tasks[i][ORDER] + '</td>' +
+                '<td>' + current_tasks[i][STAGE] + '</td>' +
+                '<td>' + current_tasks[i][TASK] + '</td>' +
+                '<td>' + current_tasks[i][GND] + '</td>' +
+                '<td>' + current_tasks[i][FLIGHT] + '</td>' +
+                '<td>' + current_tasks[i][PARENT] + '</td>' +
+                '<td><input type="checkbox" id="m_select_' + current_tasks[i][ORDER] + '" value="0" onClick= "this.value ^= true" class = "m_select"></td></tr>');
+
+            $('#n_form tbody').append(
+                '<tr><td>' + current_tasks[i][STAGE] + '</td>' +
+                '<td>' + current_tasks[i][TASK] + '</td>' +
+                '<td>' + current_tasks[i][GND] + '</td>' +
+                '<td><input type="checkbox" id="n_gnd_' + current_tasks[i][ORDER] + '" value="0" onClick= "this.value ^= true" class = "n_gnd"></td>' +
+                '<td><div data-role="fieldcontain"><input type="date" name="name" class = "n_gnd_date" id="n_gnd_date_' + current_tasks[i][ORDER] + '"value=""/></div></td>' +
+                '<td>' + current_tasks[i][FLIGHT] + '</td>' +
+                '<td><input type="checkbox" id="n_flight_' + current_tasks[i][ORDER] + '" value="0" onClick= "this.value ^= true" class = "n_flight"></td>' +
+                '<td><div data-role="fieldcontain"><input type="date" name="name" class = "n_flight_date" id="n_flight_date_' + current_tasks[i][ORDER] + '"value=""/></div></td>' +
+                '<td><input type="text" class = "n_remarks" id="n_remarks_' + current_tasks[i][ORDER] + '"></td>' +
+                '<td><input type="checkbox" id="n_select_' + current_tasks[i][ORDER] + '" value="0" onClick= "this.value ^= true" class = "n_select"></td></tr>');
+        }
     });
+}
 
-    //task List Delete
-    $('#m_delete').click(function() {
+$(document).ready(function () {
+    select_tasks();
+    $('#m_delete').click(function () {
         $('<div>').simpledialog2({
             mode: 'button',
             headerText: 'Warning',
@@ -116,10 +177,10 @@ $(document).ready(function() {
             buttonPrompt: 'Delete task(s)?',
             buttons: {
                 'OK': {
-                    click: function() {
+                    click: function () {
                         $('#buttonoutput').text('OK');
 
-                        $('.delete_tasks').each(function() {
+                        $('.m_select').each(function () {
                             if ($(this).val() === '1') {
 
                                 delete_task($(this).attr("id").substring(9));
@@ -130,7 +191,7 @@ $(document).ready(function() {
                     }
                 },
                 'Cancel': {
-                    click: function() {
+                    click: function () {
                         $('#buttonoutput').text('Cancel');
                     },
                     icon: "delete",
